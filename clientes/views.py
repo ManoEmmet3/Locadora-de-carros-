e3017b7b188998b10b3cb979373d3d884f4d1b55
emerson tiago <manoemmet@gmail.com>
2023-12-12 11:43:51 -0300

    primeiro commit

---
+++ clientes/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def clientes(request):
+    if request.method == "GET":
+        clientes_list = Cliente.objects.all()
+        return render(request, 'clientes.html', {'clientes': clientes_list})
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        sobrenome = request.POST.get('sobrenome')
+        email = request.POST.get('email')
+        cpf = request.POST.get('cpf')
+        carros = request.POST.getlist('carro')
+        placas = request.POST.getlist('placa')
+        anos = request.POST.getlist('ano')    
