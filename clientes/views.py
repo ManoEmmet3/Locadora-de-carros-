a812f644ae5aeaa956aa2270d9771f662d82a260
ManoEmmet3 <manoemmet3@gmail.com>
2023-12-16 15:50:33 -0300

    quinto commit

--- clientes/views.py
+++ clientes/views.py
@@ -79,3 +79,25 @@ def excluir_carro(request, id):
         return redirect(reverse('clientes')+f'?aba=att_cliente&id_cliente={id}')
     except:
         return redirect(reverse('clientes')+f'?aba=att_cliente&id_cliente={id}')
+    
+
+
+
+def update_cliente(request, id):
+    body = json.loads(request.body)
+
+    nome = body['nome']
+    sobrenome = body['sobrenome']
+    email = body['email']
+    cpf = body['cpf']
+
+    cliente = get_object_or_404(Cliente, id=id)
+    try:
+        cliente.nome = nome
+        cliente.sobrenome = sobrenome
+        cliente.email = email
+        cliente.cpf = cpf
+        cliente.save()
+        return JsonResponse({'status': '200', 'nome': nome, 'sobrenome': sobrenome, 'email': email, 'cpf': cpf})
+    except:
+        return JsonResponse({'status': '500'})    
